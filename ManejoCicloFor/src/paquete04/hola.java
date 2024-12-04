38047981346deea9494835dac18a2786ae455e64
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-12-04 12:04:39 -0500

    Hecho

--- ManejoCicloFor/src/paquete04/hola.java
+++ ManejoCicloFor/src/paquete04/hola.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete04;
-
+import java.util.Scanner;
 /**
  *
  * @author utpl
  */
 public class hola {
+    public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+    int contador = 0;
+   
+    System.out.println("Ingrese el número de tablas que desea crear");
+    int numero = leer.nextInt();
+    System.out.println("Ingrese el límite de la tabla");
+    int limite = leer.nextInt();
+    
+    while (contador <= limite) {}
+        System.out.println(numero + "*" + contador + contador*numero);
+        
+        
+    }
     
 }
