bfedd8b9d4d78bde2d14bb336e49965bc50b246f
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-12-04 12:14:07 -0500

    Hechooooo

--- ManejoCicloFor/src/paquete04/hola.java
+++ ManejoCicloFor/src/paquete04/hola.java
@@ -3,29 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete04;
+
 import java.util.Scanner;
+
 /**
  *
  * @author utpl
  */
 public class hola {
+
     public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
-    int contador = 1;
-    int contador2 = 0;
-    System.out.println("Ingrese el número de tablas que desea crear");
-    int tablas = leer.nextInt();
-    System.out.println("Ingrese el límite de la tabla");
-    int limite = leer.nextInt();
-    
-    while (contador <= tablas){
-        System.out.println("Tabla del:" + contador);
-        contador2 = 1;
-        contador ++;
-    while (contador2 <= limite) {
-        System.out.println(contador + "*" + contador2 + "=" + contador*contador2);
-        contador2 ++;
+        Scanner leer = new Scanner(System.in);
+        int contador = 1;
+        int contador2 = 0;
+        System.out.println("Ingrese el número de tablas que desea crear");
+        int tablas = leer.nextInt();
+        System.out.println("Ingrese el límite de la tabla");
+        int limite = leer.nextInt();
+
+        while (contador <= tablas) {
+            System.out.println("Tabla del:" + contador);
+            contador2 = 1;
+            contador++;
+            while (contador2 <= limite) {
+                System.out.println(contador + "*" + contador2 + "=" + contador * contador2);
+                contador2++;
+            }
+        }
     }
-    }
-}
 }
