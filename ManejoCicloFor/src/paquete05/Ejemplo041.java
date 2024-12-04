7910f1d3612e493f14d5311d2c86c2bb525c4687
Villa <bryan.m.villa.cgmail.com>
2024-12-04 11:31:59 -0500

    cambio

---
+++ ManejoCicloFor/src/paquete05/Ejemplo041.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete05;
+
+import paquete01.*;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo041 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i = 20; i >= 0; i-=2) {
+            System.out.printf("%d\n", i);
+        }
+      
+    }
+    
+}
