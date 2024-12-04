e451b84de8adcda4445e62aa24fe081b1ad9e08b
Villa <bryan.m.villa.cgmail.com>
2024-12-04 11:37:17 -0500

    cambio

--- ManejoCicloFor/src/paquete01/Ejemplo011.java
+++ ManejoCicloFor/src/paquete01/Ejemplo011.java
@@ -17,7 +17,7 @@ public class Ejemplo011 {
     public static void main(String[] args) {
         
         for (int i = 10; i >= 0; i--) {
-            System.out.printf("%d\n", i);
+            System.out.printf("numero: %d\n", i);
         }
         
         
